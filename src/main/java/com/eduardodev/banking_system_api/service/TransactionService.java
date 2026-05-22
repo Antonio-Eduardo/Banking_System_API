56232544763601fefefc8fb44626d2861dc34ffb
Antonio Eduardo Moreira Oliveira <119017563+Antonio-Eduardo@users.noreply.github.com>
2026-05-22 11:30:56 -0300

    feat: terminando repositories

--- src/main/java/com/eduardodev/banking_system_api/service/TransactionService.java
+++ src/main/java/com/eduardodev/banking_system_api/service/TransactionService.java
@@ -1,4 +1,29 @@
 package com.eduardodev.banking_system_api.service;
 
+import com.eduardodev.banking_system_api.entities.Transacao;
+import com.eduardodev.banking_system_api.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
 public class TransactionService {
+
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    public Transacao createTransaction(Transacao transacao) {
+        return transactionRepository.save(transacao);
+}
+        public void deleteTransaction(Long id) {
+            transactionRepository.deleteById(id);
+        }
+        public Transacao findTransactionById(Long id) {
+            return transactionRepository.findById(id).orElse(null);
+        }
+        public Transacao updateTransaction(Transacao transacao) {
+            return transactionRepository.save(transacao);
+        }
+        public Set<Transacao> findAllTransactions() {
+            return Set.copyOf(transactionRepository.findAll());
+        }
 }
