eed4a24a1388e8f2b35bffd6bdd324026d572417
Antonio Eduardo Moreira Oliveira <eduardo_m_cdc@hotmail.com>
2026-06-04 23:44:54 -0300

    fix: adicionando IF NOT EXISTS nas migrations

--- src/main/resources/db/migration/V2__add_new_columns.sql
+++ src/main/resources/db/migration/V2__add_new_columns.sql
@@ -11,3 +11,12 @@ UPDATE public.transacao SET tipo_operacao = '2' WHERE tipo_operacao = 'OPERACAO_
 UPDATE public.transacao SET tipo_operacao = '3' WHERE tipo_operacao = 'OPERACAO_TRANSFERENCIA';
 ALTER TABLE public.transacao
     ALTER COLUMN tipo_operacao TYPE INTEGER USING tipo_operacao::integer;
+
+ALTER TABLE public.conta ADD COLUMN IF NOT EXISTS razao_social VARCHAR(255);
+ALTER TABLE public.conta ADD COLUMN IF NOT EXISTS cnpj VARCHAR(255);
+ALTER TABLE public.conta ADD COLUMN IF NOT EXISTS agencia VARCHAR(255);
+ALTER TABLE public.conta ADD COLUMN IF NOT EXISTS numero_conta VARCHAR(255);
+ALTER TABLE public.conta ADD COLUMN IF NOT EXISTS limite_cheque_especial NUMERIC(10,2);
+ALTER TABLE public.conta ADD COLUMN IF NOT EXISTS data_abertura DATE;
+ALTER TABLE public.conta ADD COLUMN IF NOT EXISTS ativa BOOLEAN;
+ALTER TABLE public.conta ADD COLUMN IF NOT EXISTS data_aniversario DATE;
